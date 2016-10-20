96fcc00729d9136e3232e8ce19fd91f89b39a122
Angela1997 <ayvazyan.angela@mail.ru>
2016-10-20 23:45:16 +0400

    implementation of the other operations

--- script.js
+++ script.js
@@ -1,80 +1,25 @@
 /**
  * Created by HP on 10/20/2016.
  */
-//
-// function addition() {
-//     var x = document.getElementById("first_number").value;
-//     var y = document.getElementById("second_number").value;
-//     var z = parseFloat(x) + parseFloat(y);
-//     document.getElementById("demo").value = z;
-// }
-// function subtraction() {
-//     var x = document.getElementById("first_number").value;
-//     var y = document.getElementById("second_number").value;
-//     var z = parseFloat(x) - parseFloat(y);
-//     document.getElementById("demo").value = z;
-// }
-// function multiplication() {
-//     var x = document.getElementById("first_number").value;
-//    var y = document.getElementById("second_number").value;
-//     var z = parseFloat(x) * parseFloat(y);
-//    document.getElementById("demo").value = z;
-// }
-// function division() {
-//     var x = document.getElementById("first_number").value;
-//     var y = document.getElementById("second_number").value;
-//     var z = parseFloat(x) / parseFloat(y);
-//    document.getElementById("demo").value = z;
-//    var k = document.getElementById("demo").value;
-//     if (k = "Infinity") {
-//        document.getElementById("demo").value = "Error";
-//     }
-// }
-
-
-
-
-// function myFunction5() {
-//     var x = document.getElementById("first_number").value;
-//     var y = document.getElementById("second_number").value;
-//     var z = parseFloat(x) + parseFloat(y);
-//     var k = parseFloat(x) - parseFloat(y);
-//     var h = parseFloat(x) * parseFloat(y);
-//     var e = parseFloat(x) / parseFloat(y);
-//     document.getElementById("button1").onclick = function () {
-//         document.getElementById("demo").value = z;
-//     };
-//     document.getElementById("button2").onclick = function () {
-//         document.getElementById("demo").value = k;
-//     };
-//     document.getElementById("button3").onclick = function () {
-//         document.getElementById("demo").value = h;
-//     };
-//     document.getElementById("button4").onclick = function () {
-//         document.getElementById("demo").value = e;
-//         if (e = "Infinity") {
-//             document.getElementById("demo").value = "Error";
-//         }
-//     }
-// }
-
-
-
-// function myFunction5() {
-//     var x = document.getElementById("first_number").value;
-//     var y = document.getElementById("second_number").value;
-//     var z = parseFloat(x) + parseFloat(y);
-//     if (document.getElementById("button1").clicked == true) {
-//         alert ("a");
-//     }
-//
-//     }
-
 function myFunction5(symbol) {
-    var x = document.getElementById("first_number").value;
-    var y = document.getElementById("second_number").value;
+  var x = document.getElementById("first_number").value;
+  var y = document.getElementById("second_number").value;
 
-    if  (onclick="document.getElementById('button1')) {
-        document.getElementById("demo").value = z;
+  if (symbol == '+') {
+    document.getElementById("demo").value = parseFloat(x) + parseFloat(y);
+  }
+  else if (symbol == '-') {
+    document.getElementById("demo").value = parseFloat(x) - parseFloat(y);
+  }
+  else if (symbol == '*') {
+    document.getElementById("demo").value = parseFloat(x) * parseFloat(y);
+  }
+  else if (symbol == '/') {
+    if (y == 0) {
+      document.getElementById("demo").value = "Error";
     }
+    else {
+      document.getElementById("demo").value = parseFloat(x) / parseFloat(y);
+    }
+  }
 }
