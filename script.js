24f809b50ce38068440695989db0fcaeca724702
Anzhela Ayvazyan <angela.ayvazyan17@gmail.com>
2016-10-20 19:04:21 +0400

    implemantation of the other operations: incomplete

---
+++ script.js
@@ -0,0 +1,80 @@
+/**
+ * Created by HP on 10/20/2016.
+ */
+//
+// function addition() {
+//     var x = document.getElementById("first_number").value;
+//     var y = document.getElementById("second_number").value;
+//     var z = parseFloat(x) + parseFloat(y);
+//     document.getElementById("demo").value = z;
+// }
+// function subtraction() {
+//     var x = document.getElementById("first_number").value;
+//     var y = document.getElementById("second_number").value;
+//     var z = parseFloat(x) - parseFloat(y);
+//     document.getElementById("demo").value = z;
+// }
+// function multiplication() {
+//     var x = document.getElementById("first_number").value;
+//    var y = document.getElementById("second_number").value;
+//     var z = parseFloat(x) * parseFloat(y);
+//    document.getElementById("demo").value = z;
+// }
+// function division() {
+//     var x = document.getElementById("first_number").value;
+//     var y = document.getElementById("second_number").value;
+//     var z = parseFloat(x) / parseFloat(y);
+//    document.getElementById("demo").value = z;
+//    var k = document.getElementById("demo").value;
+//     if (k = "Infinity") {
+//        document.getElementById("demo").value = "Error";
+//     }
+// }
+
+
+
+
+// function myFunction5() {
+//     var x = document.getElementById("first_number").value;
+//     var y = document.getElementById("second_number").value;
+//     var z = parseFloat(x) + parseFloat(y);
+//     var k = parseFloat(x) - parseFloat(y);
+//     var h = parseFloat(x) * parseFloat(y);
+//     var e = parseFloat(x) / parseFloat(y);
+//     document.getElementById("button1").onclick = function () {
+//         document.getElementById("demo").value = z;
+//     };
+//     document.getElementById("button2").onclick = function () {
+//         document.getElementById("demo").value = k;
+//     };
+//     document.getElementById("button3").onclick = function () {
+//         document.getElementById("demo").value = h;
+//     };
+//     document.getElementById("button4").onclick = function () {
+//         document.getElementById("demo").value = e;
+//         if (e = "Infinity") {
+//             document.getElementById("demo").value = "Error";
+//         }
+//     }
+// }
+
+
+
+// function myFunction5() {
+//     var x = document.getElementById("first_number").value;
+//     var y = document.getElementById("second_number").value;
+//     var z = parseFloat(x) + parseFloat(y);
+//     if (document.getElementById("button1").clicked == true) {
+//         alert ("a");
+//     }
+//
+//     }
+
+function myFunction5(symbol) {
+    var x = document.getElementById("first_number").value;
+    var y = document.getElementById("second_number").value;
+
+    if  (onclick="document.getElementById('button1')) {
+        document.getElementById("demo").value = z;
+    }
+}
